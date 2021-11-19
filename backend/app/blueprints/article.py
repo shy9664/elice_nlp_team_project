9caed984a438279f913b0cc35d43543151706cd8
hoyeon <ghdus223@naver.com>
2021-11-19 10:20:00 +0900

    B: 로그인 유저 일기 목록 api test 완료

--- backend/app/blueprints/article.py
+++ backend/app/blueprints/article.py
@@ -13,7 +13,7 @@ def article(date):
     id등 특정 유저를 식별할 수 있는 것으로 가져와서
     쿼리를 다음과 같은 방식으로 로그인된 특정 유저의 해당 날짜 일기만 데이터로 가져오도록 수정.
     ex) GET, PATCH, DELETE
-    diary = Article.query.filter((Article.date == date ) & Article.author_id == id).first()
+    diary = Article.query.filter((Article.date == date ) & (Article.author_id == id)).first()
     2) POST는 AI모델이 완성되면 일기 분석 관련 로직 추가해주고 DB에 저장. 
     author 정보도 jwt에서 가져옴.
     ex)
@@ -22,7 +22,7 @@ def article(date):
 
     if request.method == 'GET':
         diary = Article.query.filter(Article.date == date).first()
-        return jsonify(date=diary.date, text=diary.text, emotion=diary.emotion, is_shared=diary.is_shared, is_sharable=diary.is_sharable)
+        return jsonify(date=diary.date.strftime('%Y-%m-%d'), text=diary.text, emotion=diary.emotion, is_shared=diary.is_shared, is_sharable=diary.is_sharable)
     
     elif request.method == 'POST':
         text = request.json['text']
@@ -70,3 +70,29 @@ def change_share_status(date, share):
     diary.is_shared = share
     db.session.commit()
     return jsonify(result='success')
+
+@my_article.route('/my-list')
+def get_my_diarys():
+    emotion = request.args.get('emotion', 'all')
+    if emotion == 'all':
+        emotion = ['joy', 'anger',]  # 추후 감정 추가되는대로 다 집어넣기
+    else:
+        emotion = [emotion]
+    share_status = request.args.get('share', 0, type=int)
+    share_status = [share_status] if share_status else [0, 1]
+    sort = request.args.get('sort', 0, type=int)
+    sort = Article.date.asc() if sort else Article.date.desc()
+    
+    # 임시 
+    id = None
+
+    query = Article.query.filter((Article.author_id == id) & (Article.emotion.in_(emotion)) & (Article.is_shared.in_(share_status))).order_by(sort)
+    my_diarys = []
+    for diary in query:
+        one_diary = {}
+        one_diary['date'] = diary.date.strftime('%Y-%m-%d')
+        one_diary['text'] = diary.text
+        one_diary['emotion'] = diary.emotion
+        one_diary['is_shared'] = diary.is_shared
+        my_diarys.append(one_diary)
+    return jsonify(my_diarys)
