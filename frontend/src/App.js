0ae0602fbd3b5fd9d297aa20ef9da656aad90661
hyun1 <wooozooo1123@gmail.com>
2021-11-24 21:55:49 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import './App.css';
 import Navigation from './components/nav';
 import UserLogin from './UserLogin/UserLogin'
@@ -6,19 +6,32 @@ import List from './routes/List';
 import Read from './routes/Read';
 import Write from './routes/Write';
 import { Route } from 'react-router';
-import Users from './Users';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        setData(response.data);
+      });
+  };
   return (
-    <div className="App">
-      <Navigation />
-      <UserLogin />
-      <Users />
+    <div>
+      <div>
+        <button onClick={onClick}>test</button>
+      </div>
+
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
 }
 
 export default App;
-
-
-
