c99ee69cc1ace26d13eec351667c00d58a3e2a89
hyun1 <wooozooo1123@gmail.com>
2021-11-24 16:50:15 +0900

    API임시

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,14 @@ import List from './routes/List';
 import Read from './routes/Read';
 import Write from './routes/Write';
 import { Route } from 'react-router';
+import Users from './Users';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <UserLogin />
+      <Users />
     </div>
   );
 }
