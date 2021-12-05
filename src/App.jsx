8ffdbd784e625a933390153c43d4512cf375bf29
zerosheepmoo <zerosheepmoo@gmail.com>
2021-12-05 10:43:45 +0900

    열린게시판 화면

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import FOF from "./pages/FOF";
 import DiaryDetails from "./pages/DiaryDetails";
 import DiaryList from "./pages/DiaryList";
 import SympDiaryList from "./pages/SympDiaryList";
+import OpenBoard from "./pages/OpenBoard";
 
 const App = () => {
     return (
@@ -26,6 +27,7 @@ const App = () => {
                 <Route path="diary/write" element={<DiaryWrite />} />
                 <Route path="diary/done" element={<DiaryDetails />} />
                 <Route path="diary/symp" element={<SympDiaryList />} />
+                <Route path="diary/opened" element={<OpenBoard />} />
                 <Route path="*" element={<FOF />} />
             </Routes>
         </>
