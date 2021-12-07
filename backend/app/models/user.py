c4db1d2f21b57b0634b25fb7de6a70ae731e0205
Kwon <anstjdrnjs3@gmail.com>
2021-12-07 16:01:39 +0900

    B: User 데이터베이스에 photo in init

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,14 +1,16 @@
-from app import db 
+from app import db
+
 
 class User(db.Model):
-    __tablename__ = 'User'
+    __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     nickname = db.Column(db.String(255), nullable=False)
     photo = db.Column(db.String(255), nullable=True)
 
-    def __init__(self, email, password, nickname):
+    def __init__(self, email, password, nickname, photo):
         self.email = email
         self.password = password
         self.nickname = nickname
+        self.photo = photo
