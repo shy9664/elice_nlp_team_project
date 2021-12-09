37fb7d46814494901c51cf45f021d5c16fd2777d
Kwon <anstjdrnjs3@gmail.com>
2021-12-10 00:02:15 +0900

    B: node_modules 삭제

--- frontend/node_modules/@udecode/plate-image-ui/dist/ImageElement/ImageElement.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { ImageElementProps } from './ImageElement.types';
-export declare const ImageElement: (props: ImageElementProps) => JSX.Element;
-//# sourceMappingURL=ImageElement.d.ts.map
--- frontend/node_modules/@udecode/plate-image-ui/dist/ImageElement/ImageElement.styles.d.ts
+++
@@ -1,5 +0,0 @@
-import { ImageElementStyleProps } from './ImageElement.types';
-export declare const getImageElementStyles: (props: ImageElementStyleProps) => {
-    root: import("@udecode/plate-styled-components").Style;
-} & Record<keyof import("./ImageElement.types").ImageElementStyles | "root", import("@udecode/plate-styled-components").Style | undefined>;
-//# sourceMappingURL=ImageElement.styles.d.ts.map
