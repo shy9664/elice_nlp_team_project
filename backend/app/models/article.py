2635c374b538261f67dc0de8ae9d983c01cda055
hoyeon <ghdus223@naver.com>
2021-12-09 22:30:01 +0900

    코드 수정

--- backend/app/models/article.py
+++ backend/app/models/article.py
@@ -5,13 +5,9 @@ from app import db
 class Article(db.Model):
     __tablename__ = "Article"
     id = db.Column(db.Integer, primary_key=True)
-<<<<<<< HEAD
-    author_id = db.Column(db.Integer, db.ForeignKey('User.id', ondelete='CASCADE'))
-=======
     author_id = db.Column(
         db.Integer, db.ForeignKey("User.id", ondelete="CASCADE"), nullable=False
     )
->>>>>>> dev
     author = db.Column(db.String(255), nullable=False)
     text = db.Column(db.Text(), nullable=False)
     date = db.Column(db.Date(), nullable=False)
@@ -19,12 +15,7 @@ class Article(db.Model):
     is_sharable = db.Column(db.Boolean(), nullable=False)
     is_shared = db.Column(db.Boolean(), nullable=False)
 
-<<<<<<< HEAD
-
-    def __init__(self, author, text, date, emotion, is_sharable, is_shared):
-=======
     def __init__(self, author, author_id, text, date, emotion, is_sharable, is_shared):
->>>>>>> dev
         self.author = author
         self.text = text
         self.date = date
